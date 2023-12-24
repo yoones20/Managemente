c9ef7f5fa4e68c66cc0f5607a2950ab2b8c40e11
yoones20 <yoonesakbari3@gmail.com>
2023-12-24 23:45:06 +0330

    TABLE teacher

--- Management1.py
+++ Management1.py
@@ -3,12 +3,16 @@ import sqlite3
 conn = sqlite3.connect("example.db")
 
 
-cursor = conn.cursor
+cursor = conn.cursor()
 
 cursor.execute("""
-CREAT TABLE IF NOT EXISTS teacher,
+CREATE TABLE IF NOT EXISTS teacher(
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
         number_class INTEGER,
-        time_class INTEGER,
-""")
+        time_class INTEGER
+ )              
+""") 
+conn.commit()
+
+conn.close()
