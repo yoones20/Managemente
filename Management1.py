df3a18d7170c19a3fcbe69c7c9008dce7c7b40a8
yoones20 <yoonesakbari3@gmail.com>
2023-12-25 17:49:47 +0330

    cursur honar-amoz

--- Management1.py
+++ Management1.py
@@ -7,12 +7,23 @@ cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS teacher(
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        number_class INTEGER,
-        time_class INTEGER
+        ID INTEGER PRIMARY KEY,
+        Name TEXT NOT NULL,
+        Number_class INTEGER,
+        Time_class INTEGER
  )              
 """) 
+
+cursor.execute("""
+CREATE TABLE  IF NOT EXISTS honar_amoz(
+        ID INTEGER PRIMARY KEY,
+        Name TEXT NOT NULL,
+        Number_class INEGER,
+        Nomre INTEGER
+)
+""")
+
+
 conn.commit()
 
 conn.close()
