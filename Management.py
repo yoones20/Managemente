97f7c46d012e7fce3a8dbc3929f526d9ff68d4d6
yoones20 <yoonesakbari3@gmail.com>
2023-12-27 22:01:06 +0330

    ffinish 2

--- Management.py
+++ Management.py
@@ -1,33 +1,77 @@
 import sys
-from PyQt5.QtSql import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
+class MainWindow(QWidget):
+    def init(self):
+        super().init()
 
-def createDB():
-   db = QSqlDatabase.addDatabase('QSQLITE')
-   db.setDatabaseName('mojtama.db')
+        self.setWindowTitle("Database GUI Example")
+        self.setGeometry(100, 100, 400, 200)
 
-   if not db.open():
-      msg = QMessageBox()
-      msg.setIcon(QMessageBox.Critical)
-      msg.setText("Error in Database Creation")
-      retval = msg.exec_()
-      return False
-query = QSqlQuery()
+        self.init_ui()
 
+    def init_ui(self):
+        
+        self.createDB()
+        self.name_label = QLabel("Name:")
+        self.name_edit = QLineEdit()
+        self.number_class_label = QLabel("Number of Classes:")
+        self.number_class_edit = QLineEdit()
+        self.time_class_label = QLabel("Time of Classes:")
+        self.time_class_edit = QLineEdit()
+        self.submit_button = QPushButton("Submit")
+        self.submit_button.clicked.connect(self.insert_data)
 
-query.exec_("""
-CREATE TABLE IF NOT EXSIST teache(
-            id INTEGER PRYMARY KEY,
-            Name TEXT NOT NULL,
-            Number_class INTEGER,
-            Time_class INTEGER
-)
-""")
+        layout = QVBoxLayout()
+        layout.addWidget(self.name_label)
+        layout.addWidget(self.name_edit)
+        layout.addWidget(self.number_class_label)
+        layout.addWidget(self.number_class_edit)
+        layout.addWidget(self.time_class_label)
+        layout.addWidget(self.time_class_edit)
+        layout.addWidget(self.submit_button)
+
+        self.setLayout(layout)
+
+    def createDB(self):
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('mojtama.db')
+
+        if not db.open():
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error in Database Creation")
+            retval = msg.exec_()
+            return False
+
+        query = QSqlQuery()
+        query.exec_("""
+            CREATE TABLE IF NOT EXISTS teache(
+                id INTEGER PRIMARY KEY,
+                Name TEXT NOT NULL,
+                Number_class INTEGER,
+                Time_class INTEGER
+            )
+        """)
+
+    def insert_data(self):
+        name = self.name_edit.text()
+        number_class = int(self.number_class_edit.text())
+        time_class = int(self.time_class_edit.text())
+        query = QSqlQuery()
+        query.prepare("INSERT INTO teache (Name, Number_class, Time_class) VALUES (?, ?, ?)")
+        query.addBindValue(name)
+        query.addBindValue(number_class)
+        query.addBindValue(time_class)
+
+        if query.exec_():
+            print("Data inserted successfully.")
+        else:
+            print("Error inserting data.")
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-
-createDB()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
